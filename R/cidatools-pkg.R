028629e3c39e98158d096de5d57c509da1bc2698
bosmaG <91289606+bosmaG@users.noreply.github.com>
2025-01-30 09:49:47 -0700

    fixes pt 3 :(

--- R/cidatools-pkg.R
+++ R/cidatools-pkg.R
@@ -1,21 +1,6 @@
 # this file is for managing the NAMESPACE for functions imported only for vignettes
 
-#' @importFrom gtsummary tbl_summary
-#' @importFrom gtsummary all_categorical
-#' @importFrom gtsummary style_pvalue
-#' @importFrom gtsummary add_p
-#' @importFrom gtsummary add_overall
-#' @importFrom gtsummary add_q
-#' @importFrom gtsummary modify_header
-#' @importFrom gtsummary bold_labels
-#' @importFrom gtsummary italicize_levels
-#' @importFrom gtsummary modify_caption
-#' @importFrom gtsummary theme_gtsummary_journal
-#' @importFrom gtsummary theme_gtsummary_mean_sd
-#' @importFrom gtsummary theme_gtsummary_eda
-#' @importFrom gtsummary tbl_uvregression
-#' @importFrom gtsummary tbl_stack
-#' @importFrom gtsummary tbl_merge
+#' @import gtsummary
 #' @importFrom survival coxph
 #' @importFrom survival Surv
 #' @importFrom kableExtra kable
@@ -26,5 +11,4 @@
 #' @importFrom kableExtra kable_paper
 #' @importFrom Hmisc label
 #' @importFrom tableone CreateTableOne
-#' @import cardx
 NULL 
