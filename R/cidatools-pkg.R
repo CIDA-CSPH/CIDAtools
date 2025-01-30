f06af9a3c8be8ec72c0cd59e8d8e85624e1988cd
bosmaG <91289606+bosmaG@users.noreply.github.com>
2025-01-30 09:37:01 -0700

    fixes NAMESPACE issues for tbl vig.

---
+++ R/cidatools-pkg.R
@@ -0,0 +1,29 @@
+# this file is for managing the NAMESPACE for functions imported only for vignettes
+
+#' @importFrom gtsummary tbl_summary
+#' @importFrom gtsummary all_categorical
+#' @importFrom gtsummary style_pvalue
+#' @importFrom gtsummary add_p
+#' @importFrom gtsummary add_overall
+#' @importFrom gtsummary add_q
+#' @importFrom gtsummary modify_header
+#' @importFrom gtsummary bold_labels
+#' @importFrom gtsummary italicize_levels
+#' @importFrom gtsummary modify_caption
+#' @importFrom gtsummary theme_gtsummary_journal
+#' @importFrom gtsummary theme_gtsummary_mean_sd
+#' @importFrom gtsummary theme_gtsummary_eda
+#' @importFrom gtsummary tbl_uvregression
+#' @importFrom gtsummary tbl_stack
+#' @importFrom gtsummary tbl_merge
+#' @importFrom survival coxph
+#' @importFrom survival Surv
+#' @importFrom kableExtra kable
+#' @importFrom kableExtra kable_styling
+#' @importFrom kableExtra kbl
+#' @importFrom kableExtra kable_classic
+#' @importFrom kableExtra kable_material
+#' @importFrom kableExtra kable_paper
+#' @importFrom Hmisc label
+#' @importFrom tableone CreateTableOne
+NULL 
