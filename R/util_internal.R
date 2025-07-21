714d765832c94cabb60566f43b1d0b164e712a44
Spencer Mahaffey <Spencer.Mahaffey@cuanschutz.edu>
2025-07-21 11:23:08 -0600

    Add Functions to help find the drive path from the full project path if set.

--- R/util_internal.R
+++ R/util_internal.R
@@ -20,6 +20,62 @@ check_string_param_value <- function(value="",parameter=""){
 }
 
 
+#' Find Common portion of paths when one path is a full path and the other is partial
+#'
+#'
+#' @param full_path The full path to search for the partial path in.
+#' @param partial_path The partial path which may start any were in the full path
+#' @return The common portion of the path.
+#'
+#' @noRd
+#' @noMd
+#'
+find_common_path <- function(full_path="",partial_path=""){
+  common <- fs::path_common(c(full_path,partial_path))
+  return(common)
+}
+
+#' Find and return the beginning of the full path up to the start of the common path.
+#'
+#'
+#' @param full_path The full path to search for the partial path in.
+#' @param partial_path The partial path which may start any were in the full path.
+#' @return The beginning of the full path proceeding the start of the partial path.
+#'
+#' @noRd
+#' @noMd
+#'
+find_drive_location <- function(full_path="",common_path=""){
+  parts_full<- fs::path_split(full_path)[[1]]
+  parts_common <- fs::path_split(common_path)[[1]]
+
+  match_index <- -1
+  for (i in seq_along(parts_full)){
+    if(parts_full[i]==parts_common[0]){
+      oldI=i
+      is_match=TRUE
+      for (j in seq_along(parts_common)){
+        if(parts_common[j] != parts_common[i]){
+          is_match=FALSE
+          break
+        }
+        i <- i+1
+      }
+      if(is_match){
+        match_index<-oldI
+        break
+      }
+      i=oldI
+    }
+  }
+
+  drive_parts <- head(parts_common,match_index)
+  drive_path <- fs::path_join(drive_parts)
+
+  return(drive_path)
+}
+
+
 #' Deprecation Warning for functions flagged to be deprecated
 #' deprecation_warn() - print a warning that the function will be deprecated.
 #'
