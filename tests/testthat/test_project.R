0c33dee81146e44cfa65d77234db8be966fa950c
Spencer Mahaffey <Spencer.Mahaffey@ucdenver.edu>
2025-11-04 15:10:30 -0700

    Adding project.R testing Updates to testing project_internal.R

--- tests/testthat/test_project.R
+++ tests/testthat/test_project.R
@@ -1,23 +1,114 @@
 home_dir <- fs::path_home()
-test_drive_dir <- "~/test_drive/"
+test_drive_dir <- "~/test_drive"
 fs::dir_create(path=test_drive_dir)
-test_project_path <- "BRANCHES/dr_test/test_project"
-full_project_dir <- paste(test_drive_dir,test_project_path,"/.ProjData/",sep="")
-fs::dir_create(path= full_project_dir)
+test_project_path <- "BRANCHES/dr_test/test_project/"
 
-#test_that("test get project drive path", {
-#  expect_error(path <- get_project_drive_path(),"Nothing found at /Volumes/dept || SPH || SPH-CIDA || CIDA Please ensure drive is mounted and you have entered your password to access the drive (and are logged into the VPN if needed.) If still experiencing issues try set_project_data_path() or set_global_default_path()")
+unlink(paste(test_drive_dir,"/",test_project_path,sep=""))
+unlink(paste(test_drive_dir,"/","BRANCHES/dr_test/",sep=""))
 
-  #w <- capture_warnings(path <- get_project_drive_path())
-  #expect_match(w,)
-  #expect_match(w, "\\.ProjData directory not found in project\\.", all = FALSE)
-  #expect_match(w, "\\.ProjData directory not found in project\\.", all = FALSE)
-  #expect_match(w,"\\.ProjData/Data.dcf File does not exist\\.", all=FALSE)
-  #expect_match(w,"get_project_meta_data\\(default_full_path_to_project\\) returned NULL project data\\.", all=FALSE)
-
-  #expect_equal(path,"")
+full_path <- paste0(test_drive_dir,"/",test_project_path)
 
 
 
+test_that("test project creation",{
+  create_project(path=full_path,
+                 project_name="Test Project", pi="Dr. Test",analyst ="Test Name",
+                 data_location ="BRANCHES/dr_test/test_project",
+                 git_location="CIDA/Test_test_project" )
+  expect_equal(file.exists(paste0(full_path,"/Admin/ReadMe.md")), TRUE)
+  expect_equal(file.exists(paste0(full_path,"/Background/ReadMe.md")), TRUE)
+  expect_equal(file.exists(paste0(full_path,"/Code/ReadMe.md")), TRUE)
+  expect_equal(file.exists(paste0(full_path,"/DataProcessed/ReadMe.md")), TRUE)
+  expect_equal(file.exists(paste0(full_path,"/DataRaw/ReadMe.md")), TRUE)
+  expect_equal(file.exists(paste0(full_path,"/Dissemination/ReadMe.md")), TRUE)
+  expect_equal(file.exists(paste0(full_path,"/ReadMe.md")), TRUE)
+  expect_equal(file.exists(paste0(full_path,"/Reports/ReadMe.md")), TRUE)
+  expect_equal(dir.exists(paste0(full_path,"/.ProjData")), TRUE)
+  expect_equal(file.exists(paste0(full_path,"/.ProjData/Data.dcf")), TRUE)
+  expect_equal(file.exists(paste0(full_path,"/test_project.Rproj")), TRUE)
+  expect_equal(file.exists(paste0(full_path,"/.Rprofile")), TRUE)
+})
 
-#  })
+test_that("test open_project",{
+  open_project(remote_project_folder=full_path)
+  expect_equal(as.character(options("cida_tools.remote_current_project_path")), full_path)
+})
+
+test_that("test project meta data",{
+  options(cida_tools.remote_current_project_path= "")
+
+  # Process the DCF file using your function
+  w <- capture_warnings(github <- get_project_github())
+  expect_match(w,".ProjData directory not found in project.",all=FALSE)
+  expect_match(w,".ProjData/Data.dcf File does not exist.",all=FALSE)
+  expect_equal(github, "")
+
+  w <- capture_warnings(proj_name <- get_project_name())
+  expect_match(w,".ProjData directory not found in project.",all=FALSE)
+  expect_match(w,".ProjData/Data.dcf File does not exist.",all=FALSE)
+  expect_equal(proj_name, "")
+  w <- capture_warnings(pi <- get_project_pi())
+  expect_match(w,".ProjData directory not found in project.",all=FALSE)
+  expect_match(w,".ProjData/Data.dcf File does not exist.",all=FALSE)
+  expect_equal(pi, "")
+  w <- capture_warnings(analyst <- get_project_analyst())
+  expect_match(w,".ProjData directory not found in project.",all=FALSE)
+  expect_match(w,".ProjData/Data.dcf File does not exist.",all=FALSE)
+  expect_equal(analyst,"")
+  w <- capture_warnings(location <- get_project_location())
+  expect_match(w,".ProjData directory not found in project.",all=FALSE)
+  expect_match(w,".ProjData/Data.dcf File does not exist.",all=FALSE)
+  expect_equal(location,"")
+
+  open_project(remote_project_folder=full_path)
+  # Process the DCF file using your function
+  github <- get_project_github()
+  proj_name <- get_project_name()
+  pi <- get_project_pi()
+  analyst <- get_project_analyst()
+  location <- get_project_location()
+  # Expectations
+  expect_equal(github, "CIDA/Test_test_project")
+  expect_equal(proj_name, "Test Project")
+  expect_equal(pi, "Dr. Test")
+  expect_equal(analyst,"Test Name")
+  expect_equal(location,"BRANCHES/dr_test/test_project/")
+
+  options(cida_tools.remote_current_project_path= "")
+
+  setwd(full_path)
+  # Process the DCF file using your function
+  github <- get_project_github()
+  proj_name <- get_project_name()
+  pi <- get_project_pi()
+  analyst <- get_project_analyst()
+  location <- get_project_location()
+  # Expectations
+  expect_equal(github, "CIDA/Test_test_project")
+  expect_equal(proj_name, "Test Project")
+  expect_equal(pi, "Dr. Test")
+  expect_equal(analyst,"Test Name")
+  expect_equal(location,"BRANCHES/dr_test/test_project/")
+
+})
+
+test_that("get project drive path",{
+  open_project(remote_project_folder=full_path)
+  drive_path <- get_project_drive_path()
+  expect_equal(drive_path,test_drive_dir)
+
+  options(cida_tools.remote_current_project_path= "")
+  w <- capture_warnings(drive_path <- get_project_drive_path())
+  #expect_match(w,".ProjData directory not found in project.",all=FALSE)
+  expect_match(w,"Nothing found at /Volumes/dept || SPH || SPH-CIDA || BRANCHES Please ensure drive is mounted and you have entered your password to access the drive \\(and are logged into the VPN if needed.\\) If still experiencing issues try set_project_data_path\\(\\) or  set_global_default_path\\(\\)",perl=TRUE,all=FALSE)
+  expect_equal(drive_path,"")
+
+})
+
+test_that("test project .Rprofile",{
+
+})
+
+
+
+unlink(paste(test_drive_dir,"/",test_project_path,sep=""))
--- tests/testthat/test_project_internal.R
+++ tests/testthat/test_project_internal.R
@@ -2,9 +2,9 @@ home_dir <- fs::path_home()
 
 global_path <-fs::path_join(c(home_dir,"/cida_defaults.dcf"))
 
-test_drive_dir <- "~/test_drive/"
+test_drive_dir <- "~/test_drive"
 fs::dir_create(path=test_drive_dir)
-test_project_path <- "BRANCHES/dr_test/test_project"
+test_project_path <- "BRANCHES/dr_test/test_project5"
 full_project_dir <- paste(test_drive_dir,test_project_path,"/.ProjData/",sep="")
 fs::dir_create(path= full_project_dir)
 
@@ -15,21 +15,21 @@ test_that("test the get_default_path function",{
   expect_match(w,".ProjData directory not found in project.",all=FALSE)
 
   #Create Global Default File
-  dcf_content <- list( path="~/test_drive2", analyst_name="Analyst Name")
+  dcf_content <- list( path="~/test_drive", analyst_name="Analyst Name")
   write.dcf(dcf_content, file.path(global_path))
   w <- capture_warnings(test_path <- get_default_path())
-  full_path_test <- as.character(fs::path_abs("~/test_drive2"))
+  full_path_test <- as.character(fs::path_abs("~/test_drive"))
   expect_equal(test_path,full_path_test)
 
   #Create Project MetaData File
   setwd(paste(test_drive_dir,test_project_path,sep=""))
-  dcf_content <- list( ProjectName="Test Project 5", PI="Dr. Test",analyst="Analyst Name",datalocation="Branches/dr_test/test_project",gitlocation="CIDATools/Test_test_project",default_full_path_to_project="~/test_drive3/Branches/dr_test/test_project")
+  dcf_content <- list( ProjectName="Test Project 5", PI="Dr. Test",analyst="Analyst Name",datalocation="Branches/dr_test/test_project5",gitlocation="CIDATools/Test_test_project5",default_full_path_to_project="~/test_drive/Branches/dr_test/test_project5")
   w <- capture_warnings(save_project_data(dcf_content))
-  expect_match(w,".ProjData/Data.dcf File does not exist.",all=FALSE)
+  #expect_match(w,".ProjData/Data.dcf File does not exist.",all=FALSE)
 
 
   w <- capture_warnings(test_path <- get_default_path())
-  full_path_test <- fs::path_abs("~/test_drive3")
+  full_path_test <- fs::path_abs("~/test_drive")
   expect_equal(test_path,full_path_test)
 
 
