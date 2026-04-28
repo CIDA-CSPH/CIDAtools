e19b18b8ba44c52c19bda34c22828c6ba74e5d53
Andrew Hill <andrew.2.hill@cuanschutz.edu>
2026-04-28 12:17:12 -0600

    Re-enable the failing setwd command for debugging.

--- tests/testthat/test_project.R
+++ tests/testthat/test_project.R
@@ -74,9 +74,10 @@ test_that("test project meta data",{
   expect_equal(analyst,"Test Name")
   expect_equal(location,"BRANCHES/dr_test/test_project/")
 
+  browser()
   options(cida_tools.remote_current_project_path= "")
 
-  #setwd(full_path)
+  setwd(full_path)
   # Process the DCF file using your function
   github <- get_project_github()
   proj_name <- get_project_name()
